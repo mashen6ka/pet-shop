f55ffb8de6a91d510d32154110536a9e86709fc5
mashen6ka <marislepokurova@gmail.com>
2022-07-07 10:36:10 +0300

    init db

---
+++ packages/server/migrations/1_init.sql
@@ -0,0 +1,111 @@
+CREATE TABLE "worker" (
+  "id" SERIAL PRIMARY KEY,
+  "login" varchar[32] UNIQUE NOT NULL,
+  "password" varchar[32] NOT NULL,
+  "first_name" varchar[32] NOT NULL,
+  "last_name" varchar[32] NOT NULL,
+  "middle_name" varchar[32],
+  "birthday" date,
+  "email" varchar[512] NOT NULL,
+  "phone" varchar[16],
+  "passport_series" varchar[4] NOT NULL,
+  "passport_num" varchar[6] NOT NULL,
+  "INN" varchar[12] NOT NULL,
+  "job_id" int NOT NULL,
+  "shop_point_id" int NOT NULL
+);
+
+CREATE TABLE "job" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar[32] NOT NULL
+);
+
+CREATE TABLE "shop_point" (
+  "id" SERIAL PRIMARY KEY,
+  "address" text NOT NULL,
+  "working_hours" jsonb NOT NULL
+);
+
+CREATE TABLE "client" (
+  "id" SERIAL PRIMARY KEY,
+  "login" varchar[32] UNIQUE NOT NULL,
+  "password" varchar[32] NOT NULL,
+  "first_name" varchar[32] NOT NULL,
+  "last_name" varchar[32] NOT NULL,
+  "middle_name" varchar[32],
+  "birthday" date,
+  "email" varchar[512] NOT NULL,
+  "phone" varchar[16],
+  "personal_discount" numeric(3,1)
+);
+
+CREATE TABLE "company" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar[128] NOT NULL,
+  "KPP" varchar[9] NOT NULL,
+  "INN" varchar[12] NOT NULL,
+  "address" text NOT NULL
+);
+
+CREATE TABLE "client_company" (
+  "client_id" int,
+  "company_id" int
+);
+
+CREATE TABLE "product" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar[128] NOT NULL,
+  "description" text NOT NULL,
+  "country_id" int NOT NULL,
+  "initial_price" int NOT NULL,
+  "discount" numeric(3,1)
+);
+
+CREATE TABLE "country" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar[32] NOT NULL
+);
+
+CREATE TABLE "order" (
+  "id" SERIAL PRIMARY KEY,
+  "client_id" int NOT NULL,
+  "company_id" int,
+  "status_id" int NOT NULL,
+  "created_at" timestamp DEFAULT LOCALTIMESTAMP,
+  "completed_at" timestamp,
+  "shop_point_id" int NOT NULL,
+  "price" int NOT NULL
+);
+
+CREATE TABLE "order_status" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar[32] NOT NULL
+);
+
+CREATE TABLE "order_items" (
+  "order_id" int NOT NULL,
+  "product_id" int NOT NULL,
+  "quantity" int NOT NULL DEFAULT 1
+);
+
+ALTER TABLE "worker" ADD FOREIGN KEY ("job_id") REFERENCES "job" ("id");
+
+ALTER TABLE "worker" ADD FOREIGN KEY ("shop_point_id") REFERENCES "shop_point" ("id");
+
+ALTER TABLE "client_company" ADD FOREIGN KEY ("client_id") REFERENCES "client" ("id");
+
+ALTER TABLE "client_company" ADD FOREIGN KEY ("company_id") REFERENCES "company" ("id");
+
+ALTER TABLE "product" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
+
+ALTER TABLE "order" ADD FOREIGN KEY ("client_id") REFERENCES "client" ("id");
+
+ALTER TABLE "order" ADD FOREIGN KEY ("company_id") REFERENCES "company" ("id");
+
+ALTER TABLE "order" ADD FOREIGN KEY ("status_id") REFERENCES "order_status" ("id");
+
+ALTER TABLE "order" ADD FOREIGN KEY ("shop_point_id") REFERENCES "shop_point" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "order" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id");
