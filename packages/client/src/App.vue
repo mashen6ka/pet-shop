2cd12e25f1791928ca9d89edac9b68b870698409
mashen6ka <marislepokurova@gmail.com>
2022-08-07 23:10:41 +0300

    add cart badge with products quantity

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -3,11 +3,44 @@
     <nav>
       <router-link to="/catalogue">Catalogue</router-link> |
       <router-link to="/contacts">Contacts</router-link> |
-      <router-link to="/cart">Cart</router-link>
+      <router-link to="/cart"
+        >Cart
+        <b-badge
+          v-if="totalQuantity !== 0"
+          class="pt-1"
+          variant="primary"
+          pill
+          >{{ totalQuantity }}</b-badge
+        ></router-link
+      >
+      |
+      <router-link to="/account">Account</router-link>
     </nav>
     <router-view />
   </div>
 </template>
+
+<script>
+import { BBadge } from "bootstrap-vue";
+
+export default {
+  components: {
+    BBadge,
+  },
+  computed: {
+    totalQuantity() {
+      let quantity = 0;
+      this.$store.getters["cart/CART"].forEach(
+        (item) => (quantity += item.quantity)
+      );
+      return quantity;
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
 
 <style>
 #app {
