49b95df3bd3a38a86a4c24e7c004e6c1592aafc9
mashen6ka <marislepokurova@gmail.com>
2022-08-09 12:36:46 +0300

    add Order Submit functionality

--- packages/client/src/views/CartView.vue
+++ packages/client/src/views/CartView.vue
@@ -104,13 +104,97 @@
             class="p-0 text-center"
           >
             <h4 class="my-auto text-center">
-              <b-button>Place order</b-button>
+              <b-button @click="showOrderModal()">Place order</b-button>
             </h4>
           </b-col>
         </b-row>
       </b-list-group-item>
     </b-list-group>
     <p v-if="itemsQuantity === 0">Oops! Your cart is empty for now :(</p>
+
+    <b-modal v-model="isOrderModal" size="xl" title="Submit Order">
+      <b-list-group class="m-3">
+        <b-list-group-item variant="primary">
+          <b-row align-v="center" class="m-3">
+            <b-col class="text-left">E-mail: {{ this.user.email }}</b-col>
+            <b-col class="text-center">Phone: {{ this.user.phone }}</b-col>
+            <b-col class="text-center"
+              >Name: {{ this.user.lastName }} {{ this.user.firstName }}</b-col
+            >
+            <b-col class="text-right"
+              >Total Price: {{ this.totalPrice }} ₽</b-col
+            >
+          </b-row>
+        </b-list-group-item>
+      </b-list-group>
+      <b-list-group class="m-3">
+        <b-list-group-item>
+          <b-form>
+            <b-form-group label="Shop:">
+              <b-form-select
+                required
+                v-model="form.shopId"
+                v-for="shop of shopList"
+                :key="shop.id"
+              >
+                <b-form-select-option :value="shop.id">{{
+                  shop.address
+                }}</b-form-select-option>
+              </b-form-select>
+              <b-form-invalid-feedback :state="isValidShopId">
+                Please choose a shop to take order from!
+              </b-form-invalid-feedback>
+            </b-form-group>
+
+            <b-form-group label="Company (optional):">
+              <b-form-select
+                required
+                v-model="form.companyId"
+                v-for="company of userCompanyList"
+                :key="company.id"
+              >
+                <b-form-select-option :value="null"
+                  >No company</b-form-select-option
+                >
+                <b-form-select-option :value="company.id">{{
+                  company.name
+                }}</b-form-select-option>
+              </b-form-select>
+            </b-form-group>
+          </b-form>
+        </b-list-group-item>
+      </b-list-group>
+
+      <template #modal-footer>
+        <b-button @click="submitOrder" size="sm" variant="primary">
+          Submit
+        </b-button>
+      </template>
+    </b-modal>
+
+    <div class="fixed-top" align="right">
+      <b-alert
+        style="max-width: 25%"
+        variant="success"
+        dismissible
+        fade
+        v-model="showOrderSuccess"
+      >
+        Successfully created order!
+      </b-alert>
+    </div>
+
+    <div class="fixed-top" align="right">
+      <b-alert
+        style="max-width: 25%"
+        variant="danger"
+        dismissible
+        fade
+        v-model="showOrderFailure"
+      >
+        Something went wrong, try again later :(
+      </b-alert>
+    </div>
   </div>
 </template>
 
@@ -126,6 +210,10 @@ import {
   BInputGroupPrepend,
   BRow,
   BCol,
+  BModal,
+  BFormInput,
+  BForm,
+  BAlert,
 } from "bootstrap-vue";
 
 export default {
@@ -139,6 +227,10 @@ export default {
     BInputGroupPrepend,
     BRow,
     BCol,
+    BModal,
+    BFormInput,
+    BForm,
+    BAlert,
   },
   computed: {
     itemList() {
@@ -159,10 +251,40 @@ export default {
       this.itemList.forEach((item) => (quantity += item.quantity));
       return quantity;
     },
+    userId() {
+      // ы
+      return 5;
+    },
+    user() {
+      return this.$store.getters["user/USER"];
+    },
+    userCompanyList() {
+      return this.$store.getters["user/USER_COMPANY_LIST"];
+    },
+    shopList() {
+      return this.$store.getters["shop/SHOP_LIST"];
+    },
+  },
+  mounted() {
+    Promise.all([
+      this.$store.dispatch("user/GET_USER", { id: this.userId }),
+      this.$store.dispatch("user/GET_USER_COMPANY_LIST", {
+        userId: this.userId,
+      }),
+      this.$store.dispatch("shop/GET_SHOP_LIST"),
+    ]);
   },
   data() {
     return {
+      showOrderSuccess: false,
+      showOrderFailure: false,
       disableInput: true,
+      isOrderModal: false,
+      form: {
+        companyId: null,
+        shopId: null,
+      },
+      isValidShopId: true,
     };
   },
   methods: {
@@ -171,8 +293,31 @@ export default {
       this.$store.dispatch("cart/UPDATE_CART_ITEM", { itemId, quantity });
       this.disableInput = true;
     },
-    placeOrder() {
-      //запрос серверу на создание ордера
+    showOrderModal() {
+      this.isOrderModal = true;
+    },
+    closeOrderModal() {
+      this.isOrderModal = false;
+    },
+    submitOrder() {
+      if (!this.form.shopId) {
+        this.isValidShopId = false;
+        return;
+      }
+      this.$store.dispatch("user/CREATE_ORDER", {
+        userId: this.user.id,
+        companyId: this.form.companyId,
+        statusId: 1, // подгрузить статусы
+        createdAt: new Date(Date.now()).toISOString(),
+        completedAt: null,
+        shopId: this.form.shopId,
+        price: this.totalPrice,
+        itemList: this.itemList,
+      });
+      this.isOrderModal = false;
+      // обработка ошибок блин
+      this.showOrderSuccess = true;
+      this.$store.dispatch("cart/CLEAR_CART");
     },
   },
 };
