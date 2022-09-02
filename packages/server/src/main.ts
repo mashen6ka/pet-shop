7c212da9e813beea8921bebc03897587e08c3932
mashen6ka <marislepokurova@gmail.com>
2022-09-02 17:11:11 +0300

    add response headers in order to fix CORS issue

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -37,6 +37,15 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 app.use("/img", express.static("../generator/img"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 // app.use(
 //   bodyParser.urlencoded({
